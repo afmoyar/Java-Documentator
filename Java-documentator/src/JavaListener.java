deca3442acfaa0ba183bfaf0fafb04c9bfd6f3da
afmoyar <afmoyar@unal.edu.co>
2020-06-18 17:18:36 -0500

    plantuml working

--- Java-documentator/src/JavaListener.java
+++ Java-documentator/src/JavaListener.java
@@ -2,5 +2,7 @@ public class JavaListener extends Java8ParserBaseListener {
     @Override
     public void enterNormalClassDeclaration(Java8Parser.NormalClassDeclarationContext ctx) {
         System.out.println("Clase detectada");
+
+
     }
 }
