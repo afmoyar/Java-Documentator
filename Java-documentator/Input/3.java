d42cbaa389cf14d14cd0e87fde3172da3013c52a
afmoyar <afmoyar@unal.edu.co>
2020-06-29 14:40:38 -0500

    readme update 2

--- Java-documentator/Input/3.java
+++ Java-documentator/Input/3.java
@@ -1,68 +1,4 @@
 public class Example{
-    class Carrera
-    {
-        private String nombre;
-        public Facultad facultadCarrera;
-        protected String departamento;
-        private static List<Estudiante> nuevoListaEstudiantes;
-
-        public Carrera(String nombre, Facultad facultad, String departamento, List<Estudiante> estudiantes)
-        {
-            this.nombre=nombre;
-            this.facultadCarrera=facultad;
-            this.departamento=departamento;
-            this.nuevoListaEstudiantes=estudiantes;
-        }
-        /*
-        public Carrera()
-        {
-            this.cambiarNombre("UNDEFINED");
-            this.cambiarFacultad(null);
-            this.cambiarDepartamento("UNDEFINED");
-            this.cambiarEstudiantes(null);
-        }
-        */
-        public Facultad darFacultad()
-        {
-            return facultadCarrera;
-        }
-        public void cambiarFacultad(Facultad facultad)
-        {
-            this.facultadCarrera=facultad;
-        }
-        public String darDepartamento()
-        {
-            return departamento;
-        }
-        public void cambiarDepartamento(String departamento)
-        {
-            this.departamento=departamento;
-        }
-        private static String darNombre()
-        {
-            return nombre;
-        }
-        protected void cambiarNombre(String nombre)
-        {
-            this.nombre=nombre;
-        }
-        public List<Estudiante> darEstudiantes()
-        {
-            return nuevoListaEstudiantes;
-        }
-        public void cambiarEstudiantes(List<Estudiante> estudiantes)
-        {
-            this.nuevoListaEstudiantes=estudiantes;
-        }
-        @Override
-        public String toString()
-        {
-            String cadena="Carrera: "+String.valueOf(this.darNombre())+"\n"+"Facultad: "+String.valueOf(this.darFacultad())
-                    +"\n"+"Departamento: "+String.valueOf(this.darDepartamento()+"\n"+"Lista de estudiantes: Vacio");
-            return cadena;
-        }
-
-    }
 
     class Estudiante
     {
@@ -162,4 +98,69 @@ public class Example{
         }
 
     }
+}
+
+class Carrera
+{
+    private String nombre;
+    public Facultad facultadCarrera;
+    protected String departamento;
+    private static List<Estudiante> nuevoListaEstudiantes;
+
+    public Carrera(String nombre, Facultad facultad, String departamento, List<Estudiante> estudiantes)
+    {
+        this.nombre=nombre;
+        this.facultadCarrera=facultad;
+        this.departamento=departamento;
+        this.nuevoListaEstudiantes=estudiantes;
+    }
+    /*
+    public Carrera()
+    {
+        this.cambiarNombre("UNDEFINED");
+        this.cambiarFacultad(null);
+        this.cambiarDepartamento("UNDEFINED");
+        this.cambiarEstudiantes(null);
+    }
+    */
+    public Facultad darFacultad()
+    {
+        return facultadCarrera;
+    }
+    public void cambiarFacultad(Facultad facultad)
+    {
+        this.facultadCarrera=facultad;
+    }
+    public String darDepartamento()
+    {
+        return departamento;
+    }
+    public void cambiarDepartamento(String departamento)
+    {
+        this.departamento=departamento;
+    }
+    private static String darNombre()
+    {
+        return nombre;
+    }
+    protected void cambiarNombre(String nombre)
+    {
+        this.nombre=nombre;
+    }
+    public List<Estudiante> darEstudiantes()
+    {
+        return nuevoListaEstudiantes;
+    }
+    public void cambiarEstudiantes(List<Estudiante> estudiantes)
+    {
+        this.nuevoListaEstudiantes=estudiantes;
+    }
+    @Override
+    public String toString()
+    {
+        String cadena="Carrera: "+String.valueOf(this.darNombre())+"\n"+"Facultad: "+String.valueOf(this.darFacultad())
+                +"\n"+"Departamento: "+String.valueOf(this.darDepartamento()+"\n"+"Lista de estudiantes: Vacio");
+        return cadena;
+    }
+
 }
